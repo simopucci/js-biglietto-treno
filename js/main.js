f1b3918238ba7f3ee04dfb4810cbecb0ec3184ca
simopucci <simo.pucci7@gmail.com>
2024-01-10 15:47:31 +0100

    fix: finalPrice javascript

--- js/main.js
+++ js/main.js
@@ -9,21 +9,22 @@ console.log(yourAge);
 const priceByKm = yourKm * 0.21;
 console.log(priceByKm.toFixed(2));
 
+let finalPrice;
+
 if (isNaN(yourKm) || isNaN(yourAge)) {
     alert('I dati inseriti non sono corretti');
 } else {
     if (yourAge < 18) {
         const discount20 = priceByKm * 20 / 100;
-        const finalPrice = priceByKm - discount20;
-        resultPrice.innerText = 'Il prezzo del tuo biglietto è: € ' + finalPrice.toFixed(2);
+        finalPrice = priceByKm - discount20;
     }
     else if (yourAge > 65) {
         const discount40 = priceByKm * 40 / 100;
-        const finalPrice = priceByKm - discount40;
-        resultPrice.innerText = 'Il prezzo del tuo biglietto è: € ' + finalPrice.toFixed(2);
+        finalPrice = priceByKm - discount40;
     }
     else {
-        const finalPrice = priceByKm;
-        resultPrice.innerText = 'Il prezzo del tuo biglietto è: € ' + finalPrice.toFixed(2);
+        finalPrice = priceByKm;
     }
 }
+
+resultPrice.innerText = 'Il prezzo del tuo biglietto è: € ' + finalPrice.toFixed(2);
